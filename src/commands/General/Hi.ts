33416dc3c521dd88d6395c09f7fcf445ebacf228
minato1220 <100998375+minato1220@users.noreply.github.com>
2022-05-15 15:48:47 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -19,7 +19,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const chitoge =
-			"https://media.tenor.com/videos/8a6408675b1f806290aaac4b939c5a98/mp4";
+			"https://c.tenor.com/OyYUtNifk34AAAPo/naruto-hinata.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: chitoge },
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `I don't have time to have a conversation with someone like you. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `𝙈𝙤𝙨𝙝𝙞 𝙢𝙤𝙨𝙝, 𝙢𝙮 𝙣𝙖𝙢𝙚 𝙞𝙨 𝙠𝙪𝙨𝙝𝙞𝙣𝙖 𝙪𝙯𝙪𝙢𝙖𝙞 𝙖 𝙬𝙝𝙖𝙩𝙨𝙖𝙥𝙥 𝙗𝙤𝙩 𝙘𝙧𝙚𝙖𝙩𝙚𝙙 𝙗𝙮 𝙖𝙠𝙖𝙨𝙝𝙞 (𝙢𝙞𝙣𝙖𝙩𝙤). 𝙞𝙛 𝙮𝙤𝙪 𝙬𝙖𝙣𝙣𝙖 𝙖𝙙𝙙 𝙢𝙚 𝙞𝙣 𝙮𝙤𝙪𝙧 𝙜𝙧𝙤𝙪𝙥 𝙩𝙝𝙚𝙣 𝙩𝙮𝙥𝙚 *.𝙢𝙤𝙙𝙨* 𝙖𝙣𝙙 𝙘𝙤𝙣𝙩𝙖𝙘𝙩 𝙩𝙝𝙚 𝙘𝙧𝙚𝙖𝙩𝙤𝙧. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
